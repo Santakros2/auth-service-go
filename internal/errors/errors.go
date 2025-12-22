981220b472d804025cb83c4f07711aeb150c3895
Santakros2 <borikarshrutik@gmail.com>
2025-12-22 22:47:18 +0530

    added code and error structure

--- internal/errors/errors.go
+++ internal/errors/errors.go
@@ -1,1 +1,16 @@
 package errors
+
+import "fmt"
+
+type AppError struct {
+	Code    Code
+	Message string
+	Err     error
+}
+
+func (e *AppError) Error() string {
+	if e.Err != nil {
+		return fmt.Sprintf("%s: %v", e.Message, e.Err)
+	}
+	return e.Message
+}
