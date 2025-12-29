3ad33d031c424ea360a70e36fd52baca9ecd1472
Santakros2 <borikarshrutik@gmail.com>
2025-12-29 23:05:07 +0530

    hey

--- internal/handlers/auth_handler.go
+++ internal/handlers/auth_handler.go
@@ -1,7 +1,19 @@
 package handlers
 
-import "auth-service/internal/service"
+import (
+	"auth-service/internal/service"
+	"net/http"
+)
 
-type h struct {
+type AuthHandler struct {
 	Service *service.Service
+}
+
+func NewHandler(s *service.Service) AuthHandler {
+	return AuthHandler{Service: s}
+}
+
+func (h *AuthHandler) LoginHandle(w http.ResponseWriter, r *http.Request) {
+	// var user domain.AuthUser
+
 }
